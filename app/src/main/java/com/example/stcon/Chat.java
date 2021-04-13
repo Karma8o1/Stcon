7e3a8236480590aaa4d82cb3ee3e1edab37fb5c5
Nouma <Noumanfaiz.369@gmail.com>
2021-04-13 20:47:25 +0500

    Student Consultant

--- app/src/main/java/com/example/stcon/Chat.java
+++ app/src/main/java/com/example/stcon/Chat.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,12 +26,22 @@ public class Chat extends AppCompatActivity implements View.OnClickListener{
     private DatabaseReference mUsersDatabase;
     private FirebaseUser mCurrent_user;
     private String mCurrent_state;
+    private DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        getSupportActionBar().hide();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout)findViewById(R.id.titles);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
         user = (TextView)findViewById(R.id.user);
         mCurrent_user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = mCurrent_user.getUid();
